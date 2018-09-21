0b8e27bbbfc1426bcd1a539cff4db821a41cc5ba
Anna <42617944+AnnaBeaver@users.noreply.github.com>
2018-09-21 09:41:17 -0700

    Completed

--- Test1Review/ArrayClass.java
+++ Test1Review/ArrayClass.java
@@ -1,33 +1,34 @@
 import java.util.Arrays;
+
 /**
  * Write a description of class ArrayClass here.
  * Anna
- * Test 1 Review
+ * Test 1 Review/Homework
  */
 public class ArrayClass
 {
-    //public static void main(){
-    int Array[]; //declaration
-    public ArrayClass (){
-
-    }
-
-    public void loadArray(){
-        //int Array[];
+    public static void main(){
+        //Problem 12
+        int Array[]; //declaration for 12
         Array = new int[4];//initialization
+        
+        //Problem 13
         for(int i = 0; i<Array.length; i++){
             Array[i] = (int)(Math.random()*10 +1);
         }
-    }
-    //public void printArray(){
-    public int getSum(){
+        System.out.println(Arrays.toString(Array));
+        
+        //Problem 14
         int sum = 0;
         for (int i = 0; i<Array.length; i++){
             sum = sum + Array[i];
         }
-    
-        return(sum);
+        System.out.println (sum);
+        
+        //Problem 15
+        int NewArray[];//declaration
+        NewArray = Array;
+        System.out.println(Arrays.toString(NewArray));
     }
-    
 }
 
